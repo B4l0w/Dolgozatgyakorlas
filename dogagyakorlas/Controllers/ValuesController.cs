6aecdc23dec0617916e27ac2616f4cc12e83ed55
B4l0w <csanalosib@kkszki.hu>
2024-10-20 11:09:11 +0200

    GetById kész

--- dogagyakorlas/Controllers/ValuesController.cs
+++ dogagyakorlas/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
+using System.Security.Cryptography;
 using System.Xml.Linq;
 
 namespace dogagyakorlas.Controllers
@@ -37,6 +38,26 @@ namespace dogagyakorlas.Controllers
             conn.Connection.Close();
 
             return jegyek;
+        }
+
+        [HttpGet("{azon}")]
+        public Jegyek GetById(Guid azon)
+        {
+            conn.Connection.Open();
+            string sql = $"SELECT * FROM `dolgozatok` WHERE Azon = \"{azon}\";";
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+
+            var result = new Jegyek
+            {
+                Azon = reader.GetGuid(0),
+                Jegy = reader.GetInt32(1),
+                Leiras = reader.GetString(2),
+                Createdtime = reader.GetDateTime(3),
+            };
+
+            return result;
         }
 
         [HttpPost]
--- dogagyakorlas/obj/Release/net8.0/dogagyakorlas.AssemblyInfo.cs
+++ dogagyakorlas/obj/Release/net8.0/dogagyakorlas.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("dogagyakorlas")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46bc01ff835cb25d647cc416964ff65aa39f592")]
 [assembly: System.Reflection.AssemblyProductAttribute("dogagyakorlas")]
